74f213f03b7ba71219e0fd5c4890f068199d671f
Yerulan <eturganbek01@gmail.com>
2021-04-23 21:22:42 +0600

    .

---
+++ src/main/java/kz/iitu/demo/LibraryMsApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/iitu/demo/LibraryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
